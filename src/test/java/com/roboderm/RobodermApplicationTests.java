dc7ba84185dadb3e4e65cfee6ad6dcba819900f2
RaketaBoom <77796518+RaketaBoom@users.noreply.github.com>
2024-03-04 19:31:08 +0400

    initial commit: add maven

---
+++ src/test/java/com/roboderm/RobodermApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roboderm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobodermApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
